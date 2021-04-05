c53c77d333fe2d7bc8410c603c832c84f33dcfba
Mark J Menger <mark.j.menger@gmail.com>
2021-04-05 21:42:36 +0000

    fix if

--- blue-green.tcl
+++ blue-green.tcl
@@ -1,6 +1,10 @@
 when CLIENT_ACCEPTED {
-set distribution [class match -value \"distribution\" equals bluegreen_datagroup]
-set blue_pool [class match -value \"blue_pool\" equals bluegreen_datagroup]
-set green_pool [class match -value \"green_pool\" equals bluegreen_datagroup]
-set rand [expr { rand() }]
-if { $rand < $distribution } {pool $blue_pool} else {pool $green_pool}}"}
+    set distribution [class match -value \"distribution\" equals bluegreen_datagroup]
+    set blue_pool [class match -value \"blue_pool\" equals bluegreen_datagroup]
+    set green_pool [class match -value \"green_pool\" equals bluegreen_datagroup]
+    set rand [expr { rand() }]
+    if { $rand < $distribution } 
+        {pool $blue_pool} 
+    else 
+        {pool $green_pool}
+}
