4c85675a925574a6f4e70191660848aaa05e02d5
Mark J Menger <mark.j.menger@gmail.com>
2021-04-07 03:14:10 +0000

    change to form posting

--- createrulesbuffered.sh
+++ createrulesbuffered.sh
@@ -1,6 +1,5 @@
 set -o allexport; source .env; set +o allexport
 
-
 i=0
 COOKIEJAR="$(mktemp)"
 CRUMB=$(curl -u "$bufferuser:$bufferpassword" --cookie-jar "$COOKIEJAR" "$bufferhost:$bufferport/crumbIssuer/api/xml?xpath=concat(//crumbRequestField,%22:%22,//crumb)")
@@ -13,17 +12,16 @@ do
         tenant="Test${virtualip}"
         echo "${virtualip}:${i}"
         rulename="/${tenant}/App/${tenant}_bluegreen_irule"
-        rulecontent="when CLIENT_ACCEPTED {\nset distribution [class match -value \\\"distribution\\\" equals bluegreen_datagroup]\nset blue_pool [class match -value \\\"blue_pool\\\" equals bluegreen_datagroup]\nset green_pool [class match -value \\\"green_pool\\\" equals bluegreen_datagroup]\nset rand [expr { rand() }]\nif { \$rand < \$distribution } {pool \$blue_pool} else {pool \$green_pool}}"
-
+    
         echo "create iRule"
-        curl -k --request POST \
-        -u "$bufferuser:$bufferpassword" \
+        curl -k \
         --url http://$bufferhost:$bufferport/job/icrestbuffer/buildWithParameters \
+        -u "$bufferuser:$bufferpassword" \
         --header "$CRUMB" \
         --cookie "$COOKIEJAR" \
-        --data 'ICREST_METHOD=POST' \
-        --data-urlencode 'ICREST_URI=/mgmt/tm/ltm/rule' \
-        --data "ICREST_JSON={\"name\":\"${rulename}\",\"apiAnonymous\":\"when CLIENT_ACCEPTED {\nset distribution [class match -value \\\"distribution\\\" equals bluegreen_datagroup]\nset blue_pool [class match -value \\\"blue_pool\\\" equals bluegreen_datagroup]\nset green_pool [class match -value \\\"green_pool\\\" equals bluegreen_datagroup]\nset rand [expr { rand() }]\nif { \$rand < \$distribution } {pool \$blue_pool} else {pool \$green_pool}}\"}"
+        -F ICREST_METHOD=POST \
+        -F ICREST_URI=/mgmt/tm/ltm/rule \
+        -F 'ICREST_JSON={"name":"'${rulename}'","apiAnonymous":"when CLIENT_ACCEPTED {\nset rand [expr {[TCP::client_port] % 100}]\nset distribution [class match -value \"distribution\" equals bluegreen_datagroup]\nif { $rand > $distribution }\n{pool '${greenpool}'}\n}"}'
 
         echo "create datagroup"
         curl -k --request POST \
@@ -31,9 +29,9 @@ do
         --url http://$bufferhost:$bufferport/job/icrestbuffer/buildWithParameters \
         --header "$CRUMB" \
         --cookie "$COOKIEJAR" \
-        --data 'ICREST_METHOD=POST' \
-        --data-urlencode 'ICREST_URI=/mgmt/tm/ltm/data-group/internal/' \
-        --data "ICREST_JSON={ \"name\": \"bluegreen_datagroup\",  \"partition\": \"${tenant}\", \"type\": \"string\", \"records\": [ { \"name\": \"blue_pool\", \"data\": \"/Common/Shared/blue\" }, { \"name\": \"distribution\", \"data\": \"0.5\" }, { \"name\": \"green_pool\", \"data\": \"/Common/Shared/green\" } ] }"
+        -F ICREST_METHOD=POST \
+        -F ICREST_URI=/mgmt/tm/ltm/data-group/internal/ \
+        -F 'ICREST_JSON={ "name": "bluegreen_datagroup",  "partition": "'${tenant}'", "type": "string", "records": [ { "name": "blue_pool", "data": "'${bluepool}'" }, { "name": "distribution", "data": "50" }, { "name": "green_pool", "data": "'${greenpool}'" } ] }'
 
         sleep 1
     done
